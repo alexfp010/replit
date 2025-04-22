c3619f4d1758a11d6ba5ac66e85ee1be109b3298
alexfp010 <alejandrofuentes@elcolegiodemorelos.edu.mx>
2025-04-22 09:53:24 -0600

    Add files via upload

---
+++ database.py
@@ -0,0 +1,26 @@
+# database.py
+
+productos = [
+    {
+        "id": 1,
+        "nombre": "Detergente líquido",
+        "marca": "Ariel",
+        "categoria": "Limpieza",
+        "precio": 50.0,
+        "unidades_disponibles": 100,
+        "presentacion": "Botella 1L",
+        "imagen_url": "https://ejemplo.com/detergente.jpg"
+    },
+    {
+        "id": 2,
+        "nombre": "Leche entera",
+        "marca": "Lala",
+        "categoria": "Lácteos",
+        "precio": 25.5,
+        "unidades_disponibles": 200,
+        "presentacion": "Caja 1L",
+        "imagen_url": "https://ejemplo.com/leche.jpg"
+    }
+]
+
+carrito = []
